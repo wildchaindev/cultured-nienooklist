e29d1fec8ca10cb2b436079e35f547d4aa61e877
Minh Ly <mly@udallas.edu>
2021-05-27 10:42:51 -0700

    Update dapp.js
    Removed starter text, imported images, testing out buttons for transactions

--- packages/client/src/dapp/pages/dapp.js
+++ packages/client/src/dapp/pages/dapp.js
@@ -6,6 +6,10 @@ import "../../lib/components/widgets/text-widget.js";
 import "../../lib/components/widgets/number-widget.js";
 import "../../lib/components/widgets/account-widget.js";
 import "../../lib/components/widgets/upload-widget.js";
+import sd from "../assets/img/ZooLogos/SanDiego.png";
+import bronx from "../assets/img/ZooLogos/bronx.png";
+import oak from "../assets/img/ZooLogos/Oakland.png";
+import elm from "../assets/img/ZooLogos/Elmswood.png";
 import { unsafeHTML } from "lit-html/directives/unsafe-html";
 import { LitElement, html, customElement, property } from "lit-element";
 
@@ -34,24 +38,43 @@ export default class DappPage extends LitElement {
     let content = html`
       <div class="container m-auto">
         <div class="row fadeIn mt-3 p-2 block">
-          <h2 class="text-6xl">🎉 Dappiness!</h2>
-          <p class="mt-3">
-            Your Dapp is ready, and the world is waiting for you to create
-            something amazing.
-          </p>
-          <p class="mt-3">
-            You are currently viewing your Dapp in the DappStarter Workspace which 
-            provides an easy way to switch between the Client, UI Harness and Server 
-            web apps. You can view any of these in a separate browser tab by clicking the 
-            <svg style="display:inline;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon feather feather-arrow-up-right"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg> 
-            icon at the top right of the frame (try it now).
-          </p>
+          <h2 class="text-6xl">Wildchain Prototype</h2>
           <h3 class="mt-3 text-3xl">Next Steps</h3>
           <ul class="mt-3 ml-5 list-decimal">
             <li class="mt-3">Interact with the feature modules you selected with the UI Harness</li>
             <li class="mt-3">Customize this page by editing <i>packages/client/src/pages/dapp.js</i></li>
           </ul>
+          <div class="row">
+            <div class="column">
+              <img src=${sd} width="133" alt="SDZooLogo" title="SDZoo"/>
+            </div>
+            <div class="column">
+              <img src=${sd} width="133" alt="SDZooLogo" title="SDZoo"/>
+            </div>
+            <div class="column">
+              <img src=${sd} width="133" alt="SDZooLogo" title="SDZoo"/>
+            </div>
+            <div class="column">
+              <img src=${sd} width="133" alt="SDZooLogo" title="SDZoo"/>
+            </div>
+          </div>
         </div>
+        <div class="container m-auto">
+        
+        <action-card
+          title="SanDiegoZoo"
+          description="An example of how to query an account during development"
+          action="getAccountInfo"
+          method="get"
+          fields="account"
+        >
+          <account-widget
+            field="account"
+            label="Account"
+            placeholder="Account address"
+          >
+          </account-widget>
+          </div>
       </div>
     `;
     return content;
