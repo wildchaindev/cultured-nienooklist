5737003d19431a86bec531b88ba6345f7d485dab
Minh Ly <mly@udallas.edu>
2021-06-01 09:55:55 -0700

    Generated Updates

--- packages/dapplib/src/lib/dapp-scripts.js
+++ packages/dapplib/src/lib/dapp-scripts.js
@@ -11,13 +11,38 @@ module.exports = class DappScripts {
 		return fcl.script`
 				
 				${DappScripts.injectImports(imports)}
-				pub fun main(account) : [UInt64]? {
+				pub fun main(account: Address) : [UInt64]? {
 				    let account = getAccount(account)
-				    let capability = account.getCapability(/public/NFTReceiver)
-				    let ref = capability!.borrow<&DappState.Collection>()
+				    let capability = account.getCapability(/public/NFTReceiver) 
+				    let ref = capability.borrow<&{DappState.NFTReceiver}>() ?? panic("Could not borrow account receiver reference")
 				
 				    return ref?.getIDs()
 				}
+		`;
+	}
+
+	static testGetID(imports) {
+		return fcl.script`
+				
+				// Print the NFTs owned by accounts 0x01 and 0x02.
+				${DappScripts.injectImports(imports)}
+				pub fun main() {
+				
+				    // Get both public account objects
+				    let account1 = getAccount(0x01cf0e2f2f715450)
+				
+				    // Find the public Receiver capability for their Collections
+				    let acct1Capability = account1.getCapability(/public/NFTReceiver)
+				
+				    // borrow references from the capabilities
+				    let receiver1Ref = acct1Capability.borrow<&{DappState.NFTReceiver}>()
+				        ?? panic("Could not borrow account 1 receiver reference")
+				
+				    // Print both collections as arrays of IDs
+				    log("Account 1 NFTs")
+				    log(receiver1Ref.getIDs())
+				}
+				
 		`;
 	}
 
--- packages/dapplib/src/lib/dapp-transactions.js
+++ packages/dapplib/src/lib/dapp-transactions.js
@@ -35,6 +35,76 @@ module.exports = class DappTransactions {
 		`;
 	}
 
+	static mint_test(imports) {
+		return fcl.transaction`
+				
+				${DappTransactions.injectImports(imports)}
+				transaction {
+				    // Reference to the collection that will be receiving the NFT
+				    let receiverRef: &{DappState.NFTReceiver}
+				
+				    // Reference to minter in Storage
+				    let minterRef: &DappState.NFTMinter
+				
+				    prepare(acct: AuthAccount) {
+				        // Get the owner's collection capability and borrow a referenc
+				        self.receiverRef = acct.getCapability<&{DappState.NFTReceiver}>(/public/NFTReceiver)
+				            .borrow()
+				            ?? panic("Could not borrow receiver reference")
+				        
+				        // Borrow a capability for the NFTMinter in storage
+				        self.minterRef = acct.borrow<&DappState.NFTMinter>(from: /storage/NFTMinter)
+				            ?? panic("could not borrow minter reference")
+				        // TODO: Event handling is not fully implemented
+				        //emit DappState.InitializeAccount(acct.address : String)
+				    }
+				
+				    execute {
+				        // Use the minter reference to mint an NFT, which deposits
+				        // the NFT into the collection that is sent as a parameter.
+				        self.minterRef.mintNFT(recipient: self.receiverRef)
+				        self.receiverRef.getIDs()
+				        log("NFT Minted and deposited to Admin's Collection")
+				    }
+				}
+		`;
+	}
+
+	static mint_test_log(imports) {
+		return fcl.transaction`
+				
+				${DappTransactions.injectImports(imports)}
+				transaction {
+				    // Reference to the collection that will be receiving the NFT
+				    let receiverRef: &{DappState.NFTReceiver}
+				
+				    // Reference to minter in Storage
+				    let minterRef: &DappState.NFTMinter
+				
+				    prepare(acct: AuthAccount) {
+				        // Get the owner's collection capability and borrow a referenc
+				        self.receiverRef = acct.getCapability<&{DappState.NFTReceiver}>(/public/NFTReceiver)
+				            .borrow()
+				            ?? panic("Could not borrow receiver reference")
+				        
+				        // Borrow a capability for the NFTMinter in storage
+				        self.minterRef = acct.borrow<&DappState.NFTMinter>(from: /storage/NFTMinter)
+				            ?? panic("could not borrow minter reference")
+				        // TODO: Event handling is not fully implemented
+				        //emit DappState.InitializeAccount(acct.address : String)
+				    }
+				
+				    execute {
+				        // Use the minter reference to mint an NFT, which deposits
+				        // the NFT into the collection that is sent as a parameter.
+				        self.minterRef.mintNFT(recipient: self.receiverRef)
+				        log(self.receiverRef.getIDs())
+				        log("NFT Minted and deposited to Admin's Collection")
+				    }
+				}
+		`;
+	}
+
       
       static injectImports(imports) {
         let importCode = '';
