33951ae6b60f9e8f1f9ff36ec3ec6d39992db933
Minh Ly <mly@udallas.edu>
2021-06-02 10:28:12 -0700

    Generated updates

--- packages/dapplib/src/lib/dapp-scripts.js
+++ packages/dapplib/src/lib/dapp-scripts.js
@@ -21,12 +21,10 @@ module.exports = class DappScripts {
 		`;
 	}
 
-	static testGetID(imports) {
+	static getID(imports) {
 		return fcl.script`
-				
-				// Print the NFTs owned by accounts 0x01 and 0x02.
 				${DappScripts.injectImports(imports)}
-				pub fun main() {
+				pub fun main() : [UInt64]?{
 				
 				    // Get both public account objects
 				    let account1 = getAccount(0x01cf0e2f2f715450)
@@ -39,8 +37,31 @@ module.exports = class DappScripts {
 				        ?? panic("Could not borrow account 1 receiver reference")
 				
 				    // Print both collections as arrays of IDs
-				    log("Account 1 NFTs")
-				    log(receiver1Ref.getIDs())
+				    return receiver1Ref.getIDs()
+				}
+				
+		`;
+	}
+
+	static testGetID(imports) {
+		return fcl.script`
+				
+				// Print the NFTs owned by accounts 0x01 and 0x02.
+				${DappScripts.injectImports(imports)}
+				pub fun main() : [UInt64]?{
+				
+				    // Get both public account objects
+				    let account1 = getAccount(0x01cf0e2f2f715450)
+				
+				    // Find the public Receiver capability for their Collections
+				    let acct1Capability = account1.getCapability(/public/NFTReceiver)
+				
+				    // borrow references from the capabilities
+				    let receiver1Ref = acct1Capability.borrow<&{DappState.NFTReceiver}>()
+				        ?? panic("Could not borrow account 1 receiver reference")
+				
+				    // Print both collections as arrays of IDs
+				    return receiver1Ref.getIDs()
 				}
 				
 		`;
--- packages/dapplib/src/lib/dapp-transactions.js
+++ packages/dapplib/src/lib/dapp-transactions.js
@@ -94,14 +94,56 @@ module.exports = class DappTransactions {
 				        //emit DappState.InitializeAccount(acct.address : String)
 				    }
 				
-				    execute {
+				    execute{
 				        // Use the minter reference to mint an NFT, which deposits
 				        // the NFT into the collection that is sent as a parameter.
 				        self.minterRef.mintNFT(recipient: self.receiverRef)
-				        log(self.receiverRef.getIDs())
 				        log("NFT Minted and deposited to Admin's Collection")
+				        return self.receiverRef.getIDs())
 				    }
 				}
+		`;
+	}
+
+	static transfer_test(imports) {
+		return fcl.transaction`
+				${DappTransactions.injectImports(imports)}
+				transaction {
+				
+				    // The field that will hold the NFT as it is being
+				    // transferred to the other account
+				    let transferToken: @DappState.NFT
+				
+				    let nftId: UInt64
+					
+				    prepare(acct: AuthAccount) {
+				
+				        // Borrow a reference from the stored collection
+				        let collectionRef = acct.borrow<&DappState.Collection>(from: /storage/NFTCollection)
+				            ?? panic("Could not borrow a reference to the owner's collection")
+				
+				        // Call the withdraw function on the sender's Collection
+				        // to move the NFT out of the collection
+				        self.transferToken <- collectionRef.withdraw(withdrawID: nftId)
+				    }
+				
+				    execute {
+				        // Get the recipient's public account object
+				        let recipient = getAccount(0x01)
+				
+				        // Get the Collection reference for the receiver
+				        // getting the public capability and borrowing a reference from it
+				        let receiverRef = recipient.getCapability<&{ExampleNFT.NFTReceiver}>(/public/NFTReceiver)
+				            .borrow()
+				            ?? panic("Could not borrow receiver reference")
+				
+				        // Deposit the NFT in the receivers collection
+				        receiverRef.deposit(token: <-self.transferToken)
+				
+				        log("NFT ID 1 transferred from account 2 to account 1")
+				    }
+				}
+				 
 		`;
 	}
 
